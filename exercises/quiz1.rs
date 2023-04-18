fc6cf8b9b2356818909f04b343523625d6c6a866
Kent <kentup777@gmail.com>
2023-04-18 13:14:37 -0700

    exercises and quiz 1
    	modified:   exercises/functions/functions1.rs
    	modified:   exercises/functions/functions2.rs
    	modified:   exercises/functions/functions3.rs
    	modified:   exercises/functions/functions4.rs
    	modified:   exercises/functions/functions5.rs
    	modified:   exercises/if/if1.rs
    	modified:   exercises/if/if2.rs
    	modified:   exercises/intro/intro1.rs
    	modified:   exercises/intro/intro2.rs
    	modified:   exercises/quiz1.rs
    	modified:   exercises/variables/variables1.rs
    	modified:   exercises/variables/variables2.rs
    	modified:   exercises/variables/variables3.rs
    	modified:   exercises/variables/variables4.rs
    	modified:   exercises/variables/variables5.rs
    	modified:   exercises/variables/variables6.rs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cnt: u32) -> u32 {
+    if cnt > 40 {
+	cnt
+    } else {
+	2 * cnt
+    }
+}
 
 // Don't modify this function!
 #[test]
